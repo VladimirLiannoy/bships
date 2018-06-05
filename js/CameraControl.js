f70088d225260981028cbdd13c8cd463e932e306
Vladimir Liannoy <vladimir.liannoy@netent.com>
2018-06-05 11:12:34 +0300

    minor tweaks;

--- js/CameraControl.js
+++ js/CameraControl.js
@@ -16,9 +16,10 @@ class CameraControl {
         this.addEventHandlers(domElement);
 
         this.pointer = this.addRedPointer(scene);
+        this.line = this.targetLinePointer(scene);
     }
 
-    addRedPointer(scene){
+    addRedPointer(scene) {
         var geometry = new THREE.BoxGeometry(1, 1);
         var material = new THREE.MeshNormalMaterial();
         var pointer = new THREE.Mesh(geometry, material);
@@ -26,6 +27,26 @@ class CameraControl {
         scene.add(pointer);
 
         return pointer;
+    }
+
+    targetLinePointer(scene) {
+        var material = new THREE.LineBasicMaterial({
+            color: 0x0000ff
+        });
+
+        var geometry = new THREE.Geometry();
+        geometry.vertices.push(
+            new THREE.Vector3(0, 1, 0),
+            new THREE.Vector3(0, 1, 0)
+        );
+
+        var line = new THREE.Line(geometry, material);
+
+        scene.add(line);
+
+        console.error(line);
+
+        return line;
     }
 
     addEventHandlers(domElement) {
@@ -123,6 +144,14 @@ class CameraControl {
                 //console.log(intersects[i].point.x, intersects[i].point.z);
                 me.pointer.position.x = intersects[i].point.x;
                 me.pointer.position.z = intersects[i].point.z;
+
+                me.line.geometry.vertices[0].x = shipPosition.x;
+                me.line.geometry.vertices[0].z = shipPosition.z;
+
+                me.line.geometry.vertices[1].x = intersects[i].point.x;
+                me.line.geometry.vertices[1].z = intersects[i].point.z;
+
+                me.line.geometry.verticesNeedUpdate = true;
             }
         }
 
